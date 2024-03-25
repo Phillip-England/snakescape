7c4ff209979c04ed51590de5abaf7fa026a39ee3
Phillip-England <phillip.mark.england@gmail.com>
2024-03-25 04:13:00 -0500

    added md:hidden to overlay to prevent displaying on desktop screens

--- internal/component/component_templ.go
+++ internal/component/component_templ.go
@@ -145,7 +145,7 @@ func MobileNav() templ.Component {
 	})
 }
 
-func Overlay() templ.Component {
+func MobileOverlay() templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
 		if !templ_7745c5c3_IsBuffer {
--- internal/template/template_templ.go
+++ internal/template/template_templ.go
@@ -71,7 +71,7 @@ func Base(title string) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = component.Overlay().Render(ctx, templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = component.MobileOverlay().Render(ctx, templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
